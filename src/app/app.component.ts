06ba3d19fbb56a6c0dc4ba8ff5e1f82eb1d79164
vishu39 <vipulsharmak@gmail.com>
2021-12-03 10:31:00 +0530

    forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from './user';
+import { EnrollmentService } from './enrollment.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'forms';
+  title = 'app';
+  topics = ['Angular', 'React', 'Vue'];
+  userModel = new User('Rob', 'rob@test.com', 5556665566, 'default', 'morning', true);
+  topicHasError = true;
+  submitted = false;
+  errorMsg = '';
+
+  constructor(private _enrollmentService: EnrollmentService) {}
+
+  validateTopic(value: string) {
+    if (value === 'default') {
+      this.topicHasError = true;
+    } else {
+      this.topicHasError = false;
+    }
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this._enrollmentService.enroll(this.userModel)
+      .subscribe(
+        response => console.log('Success!', response),
+        error => this.errorMsg = error.statusText
+      )
+  }
 }
